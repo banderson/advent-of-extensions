35fc984b5e852fd2a319e8a97ba08060dac035db
Ben Anderson <banderson@hubspot.com>
2024-01-12 13:37:11 -0500

    fix: remove old hardcoded test token
    It's invalidated, don't be a jerk 👿 😛

--- src/app/app.functions/projects.js
+++ src/app/app.functions/projects.js
@@ -1,8 +1,6 @@
 const hubspotClient = require("@hubspot/api-client");
 
-// const { PRIVATE_APP_ACCESS_TOKEN } = process.env;
-// const PRIVATE_APP_ACCESS_TOKEN = "a8e9d6b2-6b8d-4d7e-9f7e-7f8d9a6e5b4b";
-const PRIVATE_APP_ACCESS_TOKEN = "pat-na1-46055467-0b79-4d80-b6d1-fbbc5644b38f";
+const { PRIVATE_APP_ACCESS_TOKEN } = process.env;
 
 const client = new hubspotClient.Client({
   apiKey: PRIVATE_APP_ACCESS_TOKEN,
